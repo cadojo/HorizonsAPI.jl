1f724b7bee8bbf44028f389c1439af59c4d824a3
Joe Carpinelli <jdcarpinelli@gmail.com>
2022-05-25 22:15:23 -0500

    Adding warning to top-level docstring

--- src/HORIZONSAPI.jl
+++ src/HORIZONSAPI.jl
@@ -1,6 +1,11 @@
 """
 Retrieve ephemeris data for free using JPL's REST API!
 
+!!! warning
+    This package is not affiliated **or** endorsed by NASA, JPL, Caltech, or any other
+    organization! This is an independently written package by an astrodynamics hobbyist.
+    See this package's license file for more information about code sharing and usage!
+
 # Extended Help
 
 ## README
