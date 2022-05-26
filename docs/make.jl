c357da036d95e066413b9ed89675d0c5265a550e
Joe Carpinelli <jdcarpinelli@gmail.com>
2022-05-25 22:33:29 -0500

    Correcting docs

--- docs/make.jl
+++ docs/make.jl
@@ -8,5 +8,9 @@ makedocs(
 )
 
 deploydocs(
-    repo = "https://github.com/cadojo/HORIZONSAPI.jl"
+    target    = "build",
+    repo      = "github.com/cadojo/HORIZONSAPI.jl.git",
+    branch    = "gh-pages",
+    devbranch = "main",
+    versions  = ["stable" => "v^", "manual", "v#.#", "v#.#.#"],
 )
